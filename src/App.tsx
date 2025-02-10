c32b8a44d9126cf70963abdb5f33b6e889a3fea9
Tedeyy <tedjus666@gmail.com>
2025-02-10 08:05:55 +0800

    added page2 and contents of login, menu and page1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -33,18 +32,19 @@ import '@ionic/react/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import Menu from './pages/Menu';
+import Login from './pages/Login';
+
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/it35-lab/home">
-          <Home />
-        </Route>
-        <Route exact path="/it35-lab">
-          <Redirect to="/it35-lab/home" />
-        </Route>
+
+      <Route exact path="/it35-lab" component={Login} />
+      <Route path="/it35-lab/app" component={Menu} />
+
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
