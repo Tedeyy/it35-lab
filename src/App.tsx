1c1027baebfa81aa9e077ee242c4e1970577b141
Tedeyy <tedjus666@gmail.com>
2025-02-10 08:41:54 +0800

    added changes to menu, home and about. need tweaking

--- src/App.tsx
+++ src/App.tsx
@@ -31,9 +31,8 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
-
-import Menu from './pages/Menu';
 import Login from './pages/Login';
+import Menu from './pages/Menu';
 
 setupIonicReact();
 
@@ -41,10 +40,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-
-      <Route exact path="/it35-lab" component={Login} />
-      <Route path="/it35-lab/app" component={Menu} />
-
+        <Route exact path="/it35-lab" component={Login} />
+        <Route path="/it35-lab/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
