aacb2b5ed414fc4c59a2f6f966e4b3056ec42814
Tedeyy <tedjus666@gmail.com>
2025-03-24 08:17:51 +0800

    Updates

--- assets/swipe-back-legacy-1Zmgf_a3.js
+++ assets/swipe-back-legacy-1Zmgf_a3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DYMGfGT9.js","./index3-legacy-Bd1XMkRR.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DGbGOues.js","./index3-legacy-DWzMuE2J.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
