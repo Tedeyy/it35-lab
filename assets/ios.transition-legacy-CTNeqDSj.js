f06265c3c762b08915fec28c5ce9c048c266e554
Tedeyy <tedjus666@gmail.com>
2025-05-08 23:07:50 +0800

    Updates

--- assets/ios.transition-legacy-CTNeqDSj.js
+++ assets/ios.transition-legacy-CTNeqDSj.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DXp6-6sl.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DVIqsTsD.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-tFwU-1OT.js
+++ assets/swipe-back-legacy-tFwU-1OT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DXp6-6sl.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DVIqsTsD.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
